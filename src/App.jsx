5d9725a09b36b486b311bb93e5d67c46b2a63b9d
Ji Bing Ni <112521325+JiBing17@users.noreply.github.com>
2023-12-16 16:38:29 -0500

    updated app.jsx comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
+// Imports necessary components from respective libraries
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
 
+// Imports specific components from a local file
 import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
-// App Component
+// App Component - defines the structure of the app
 const App = () => (
+  // Sets up routing for the app using BrowserRouter
   <BrowserRouter>
+    {/* Provides a Box component with a black background */}
     <Box sx={{ backgroundColor: '#000' }}>
+      {/* Renders the Navbar component */}
       <Navbar />
+      {/* Sets up routes for different paths */}
       <Routes>
+        {/* Renders Feed component when exact path '/' is matched */}
         <Route exact path='/' element={<Feed />} />
+        {/* Renders VideoDetail component when path matches '/video/:id' */}
         <Route path='/video/:id' element={<VideoDetail />} />
+        {/* Renders ChannelDetail component when path matches '/channel/:id' */}
         <Route path='/channel/:id' element={<ChannelDetail />} />
+        {/* Renders SearchFeed component when path matches '/search/:searchTerm' */}
         <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
 );
 
+// Export the App component as the default export
 export default App;
