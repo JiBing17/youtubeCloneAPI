64d8cd22b69db95b6f19f66fddb9d9c0b9d55416
Ji Bing Ni <112521325+JiBing17@users.noreply.github.com>
2023-12-18 11:54:37 -0500

    added commnets for Loader.jsx and Navbar.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,13 +1,18 @@
+// Importing necessary components from React and Material-UI
 import React from 'react';
 import { Box, CircularProgress, Stack } from '@mui/material';
 
-// Loader Component
-const Loader = () =>  (
+// Loader Component - Displays a loading spinner while content is being loaded
+const Loader = () => (
+  // Container Box for the loader, set to minimum height of 95vh
   <Box minHeight="95vh">
-    <Stack direction='row' justifyContent='center' alignItems='center' height='80vh' >
+    {/* Stack layout for centering the CircularProgress component */}
+    <Stack direction='row' justifyContent='center' alignItems='center' height='80vh'>
+      {/* Circular loading indicator */}
       <CircularProgress />
     </Stack>
   </Box>
 );
 
+// Exporting Loader as the default component
 export default Loader;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,27 @@
+// Importing necessary components from React and Material-UI
 import React from 'react';
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 
+// Importing a constant for the logo from a local file and SearchBar component
 import { logo } from '../utils/constants';
 import { SearchBar } from "./";
 
-// Navbar Component
+// Navbar Component - Displays a navigation bar with a logo and search functionality
 const Navbar = () => {
   return (
-    <Stack direction= 'row' alignItems= 'center' p = {2} sx  = {{position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between'}}>
-        <Link to = '/' style = {{display: 'flex', alignItems: 'center' }}>
-            <img src={logo} alt="logo" height = {45} />
-        </Link>
-        <SearchBar/>
+    // Stack layout for arranging elements in a row with specific styling
+    <Stack direction='row' alignItems='center' p={2} sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}>
+      {/* Link to the home page with the logo */}
+      <Link to='/' style={{ display: 'flex', alignItems: 'center' }}>
+        {/* Displaying the logo */}
+        <img src={logo} alt="logo" height={45} />
+      </Link>
+      {/* Rendering the SearchBar component */}
+      <SearchBar />
     </Stack>
   )
 }
 
-export default Navbar
+// Exporting Navbar as the default component
+export default Navbar;
