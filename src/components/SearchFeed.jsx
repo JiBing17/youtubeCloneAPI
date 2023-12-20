c67bd291772e1c005a99c41771aad15071704bb9
Ji Bing Ni <112521325+JiBing17@users.noreply.github.com>
2023-12-20 12:39:09 -0500

    added comments to searchFeed.jsx

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,34 +1,44 @@
-import React, {useState, useEffect} from 'react'
+// Importing necessary components from React and Material-UI
+import React, { useState, useEffect } from 'react'
 import { Typography, Box } from '@mui/material'
 import Videos from './Videos'
 
+// Importing useParams and fetchFromAPI utility function from React Router and local file
 import { useParams } from 'react-router-dom'
 import { fetchFromAPI } from '../utils/fetchFromAPI'
 
-// Search Feed Component
+// SearchFeed Component - Displays videos based on the search term
 const SearchFeed = () => {
-  // keeps track of fetched videos from search
+  // State to keep track of fetched videos from search
   const [videos, setVideos] = useState(null);
   
-  //searchterm  used for dynamic route access
+  // Accessing the search term from the route parameters
   const { searchTerm } = useParams();
 
-  // fetches data whenever search term is changed 
-  useEffect(()=>{
-    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data)=> setVideos(data.items))
-  },[searchTerm]);
+  // Fetches data whenever the search term changes 
+  useEffect(() => {
+    // Fetching data based on the search term
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items));
+  }, [searchTerm]);
 
   return (
+    // Container Box with padding and minimum height
     <Box p={2} minHeight="95vh">
-      <Typography variant="h4" fontWeight={900}  color="white" mb={3} ml={{ sm: "100px"}}>
+      {/* Heading displaying the search term */}
+      <Typography variant="h4" fontWeight={900} color="white" mb={3} ml={{ sm: "100px" }}>
         Search Results for <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
       </Typography>
+      {/* Displaying videos in a Box */}
       <Box display="flex">
+        {/* Spacer Box */}
         <Box sx={{ mr: { sm: '100px' } }}/>
+        {/* Rendering Videos component with fetched videos */}
         {<Videos videos={videos} />}
       </Box>
     </Box>
   )
 }
 
-export default SearchFeed
+// Exporting SearchFeed as the default component
+export default SearchFeed;
