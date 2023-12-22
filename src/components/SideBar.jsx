d03e36b1b60ca26d8dd15f32bd2a95c1a8fe1e30
Ji Bing Ni <112521325+JiBing17@users.noreply.github.com>
2023-12-22 12:51:39 -0500

    added comments to sideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,13 @@
+// Importing necessary components from React and Material-UI
 import React from "react";
 import { Stack } from "@mui/material";
 
+// Importing categories constant from a local file
 import { categories } from "../utils/constants";
 
-//SideBar Component
+// SideBar Component - Displays categories as buttons with icons
 const SideBar = ({ selectedCategory, setSelectedCategory }) => (
+  // Stack layout for displaying categories vertically on smaller screens
   <Stack
     direction="row"
     sx={{
@@ -13,22 +16,24 @@ const SideBar = ({ selectedCategory, setSelectedCategory }) => (
       flexDirection: { md: "column" },
     }}
   >
-    {/* iterates through each category to display each name and icon as a component of the Sidebar*/}
+    {/* Mapping through each category to display as a component in the Sidebar */}
     {categories.map((category) => (
+      // Button for each category
       <button
         className="category-btn"
         onClick={() => setSelectedCategory(category.name)}
         style={{
+          // Styling based on whether the category is selected or not
           background: category.name === selectedCategory && "#FC1503",
           color: "white",
         }}
         key={category.name}
       >
-        {/* Icon */}
+        {/* Icon for the category */}
         <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>
           {category.icon}
         </span>
-        {/* Icon Name */}
+        {/* Name of the category */}
         <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
           {category.name}
         </span>
@@ -37,4 +42,5 @@ const SideBar = ({ selectedCategory, setSelectedCategory }) => (
   </Stack>
 );
 
+// Exporting SideBar as the default component
 export default SideBar;
